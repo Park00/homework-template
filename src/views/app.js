a38d93765b1fff75dc0028cf7a819746924cf564
inae <bbaaginae@gmail.com>
2021-02-04 00:50:00 +0900

    Revert "router 설정"
    This reverts commit d18b7491
    https://reacttraining.com/blog/react-router-v6-pre/#introducing-routes
    
    todo 리액트 라우터 버전6 으로 설정하

--- src/views/app.js
+++ src/views/app.js
@@ -1,13 +1,7 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
-import ROUTES, {RenderRoutes} from "../config/route";
-
 import "../assets/style.scss"
-
 export default function App(){
   return (
-    <BrowserRouter>
-      <RenderRoutes routes={ROUTES} />
-    </BrowserRouter>
+    <div>테스트</div>
   )
 }
