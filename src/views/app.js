d18b74916e19852a1e7df7724582fda0111d856e
inae <bbaaginae@gmail.com>
2021-02-03 21:50:20 +0900

    router 설정

--- src/views/app.js
+++ src/views/app.js
@@ -1,7 +1,13 @@
 import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import ROUTES, {RenderRoutes} from "../config/route";
+
 import "../assets/style.scss"
+
 export default function App(){
   return (
-    <div>테스트</div>
+    <BrowserRouter>
+      <RenderRoutes routes={ROUTES} />
+    </BrowserRouter>
   )
 }
