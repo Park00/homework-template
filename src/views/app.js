25caa344d6521d7fd5bad896a2552a1186d6182e
inae <bbaaginae@gmail.com>
2021-02-04 20:28:23 +0900

    커스텀 ul css 추가

--- src/views/app.js
+++ src/views/app.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "../assets/style.scss"
 export default function App(){
+
   return (
-    <div>테스트</div>
+    <ul className="custom-ul">
+      <li className="custom-li">들여쓰기</li>
+    </ul>
   )
 }
